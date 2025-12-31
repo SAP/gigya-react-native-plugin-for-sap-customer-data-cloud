960dea96cf8780913a254141592b05f7a2df1f0e
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2025-12-31 08:33:37 +0200

    release: version update 0.6.2 (hotfix)

--- ios/GigyaSdkWrapper.swift
+++ ios/GigyaSdkWrapper.swift
@@ -72,7 +72,7 @@ class GigyaSdkWrapper<T: GigyaAccountProtocol>: GigyaSdkWrapperProtocol {
     var currentResolver: GigyaResolverModelProtocol?
 
     init(accountSchema: T.Type) {
-        GigyaDefinitions.versionPrefix = "react_native_0.6.1_"
+        GigyaDefinitions.versionPrefix = "react_native_0.6.2_"
         gigya = Gigya.sharedInstance(accountSchema)
     }
 
